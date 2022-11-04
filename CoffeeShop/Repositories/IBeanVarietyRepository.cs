9f277cd28e390489c6433c3c87b4239a79c16583
shanedbutler <shanedbutler@gmail.com>
2022-11-04 11:00:51 -0400

    Initial commit

---
+++ CoffeeShop/Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShop.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        List<BeanVariety> GetAll();
+        BeanVariety Get(int id);
+        void Add(BeanVariety variety);
+        void Update(BeanVariety variety);
+        void Delete(int id);
+    }
+}
---
+++ js-app/main.js
@@ -0,0 +1,14 @@
+const url = "https://localhost:44359/api/beanvariety/";
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+        })
+});
+
+function getAllBeanVarieties() {
+    return fetch(url)
+        .then(resp => resp.json());
+}
